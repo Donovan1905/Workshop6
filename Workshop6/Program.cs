85074e7d55183682f21965b4dab39bb46fd93e49
Donovan <donovan.hoang@viacesi.fr>
2020-12-08 12:38:45 +0100

    add method to get free tool and assembling

--- Workshop6/Program.cs
+++ Workshop6/Program.cs
@@ -5,25 +5,44 @@ namespace Workshop6
 {
     class Program
     {
-        public Worker W1 = new Worker("W1", Enums.WorkerStatus.WAITING);
-        public Worker W2 = new Worker("W1", Enums.WorkerStatus.WAITING);
-        public Worker W3 = new Worker("W1", Enums.WorkerStatus.WAITING);
-        public Worker W4 = new Worker("W1", Enums.WorkerStatus.WAITING);
 
-        public Screwdriver Scr1 = new Screwdriver("Scr1", Enums.ToolStatus.FREE);
-        public Screwdriver Scr2 = new Screwdriver("Scr2", Enums.ToolStatus.FREE);
-        public Wrench Wrc1 = new Wrench("Wrc1", Enums.ToolStatus.FREE);
-        public Wrench Wrc2 = new Wrench("Wrc2", Enums.ToolStatus.FREE);
 
         static void Main()
         {
-            
+             Worker W1 = new Worker("W1", Enums.WorkerStatus.WAITING);
+             Worker W2 = new Worker("W2", Enums.WorkerStatus.WAITING);
+             Worker W3 = new Worker("W3", Enums.WorkerStatus.WAITING);
+             Worker W4 = new Worker("W4", Enums.WorkerStatus.WAITING);
 
-        }
+             Screwdriver Scr1 = new Screwdriver("Scr1", Enums.ToolStatus.FREE);
+             Screwdriver Scr2 = new Screwdriver("Scr2", Enums.ToolStatus.FREE);
+             Wrench Wrc1 = new Wrench("Wrc1", Enums.ToolStatus.FREE);
+             Wrench Wrc2 = new Wrench("Wrc2", Enums.ToolStatus.FREE);
 
-        public void InitializeWorkers()
+            InitializeWorkers(W1, W2, W3, W4, Wrc1, Scr1, Wrc2, Scr2);
+
+            W1.Speak();
+            W2.Speak();
+            W3.Speak();
+            W4.Speak();
+
+
+    }
+
+        public static void InitializeWorkers(Worker W1, Worker W2, Worker W3, Worker W4, Tool rightW1Tool, Tool rightW2Tool, Tool rightW3Tool, Tool rightW4Tool)
         {
+            W1.LeftNeighbor = W2;
+            W2.LeftNeighbor = W3;
+            W3.LeftNeighbor = W4;
+            W4.LeftNeighbor = W1;
 
+            W1.Tools["right"] = rightW1Tool;
+            W2.Tools["right"] = rightW2Tool;
+            W3.Tools["right"] = rightW3Tool;
+            W4.Tools["right"] = rightW4Tool;
+
+            rightW1Tool.Status = Enums.ToolStatus.USED;
+            rightW3Tool.Status = Enums.ToolStatus.USED;
         }
     }
 }
--- Workshop6/Worker.cs
+++ Workshop6/Worker.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 using Workshop6.Tools;
 using Workshop6.Enums;
+using System.Threading;
 
 namespace Workshop6
 {
+
+    /*
+     * 
+     *          ATTENTION
+     *          
+     *          
+     *     Je n'ai pas encore finis le workshop et je compte le continuer alors j'ai fais un github : https://github.com/Donovan1905/Workshop6
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
     class Worker
     {
-        private List<Tool> tools = new List<Tool>();
+        private Dictionary<string, Tool> tools = new Dictionary<string, Tool>();
 
         private string name;
 
@@ -16,10 +30,20 @@ namespace Workshop6
 
         private Worker leftNeigbhor;
 
+        private int done;
+
         public Worker(string name, WorkerStatus workerStatus)
         {
             this.Name = name;
             this.status = workerStatus;
+            this.Tools.Add("right", null);
+            this.Tools.Add("left", null);
+        }
+
+        public int Done
+        {
+            get { return this.done; }
+            set { this.done = value; }
         }
 
         public Worker LeftNeighbor
@@ -28,7 +52,7 @@ namespace Workshop6
             set { this.leftNeigbhor = value; }
         }
 
-        public List<Tool> Tools
+        public Dictionary<string, Tool> Tools
         {
             get { return this.tools; }
             set { this.tools = value; }
@@ -49,6 +73,13 @@ namespace Workshop6
         public void Assembling()
         {
             this.Status = WorkerStatus.ASSEMBLING;
+            this.Tools["left"].Status = ToolStatus.USED;
+            Thread.Sleep(4000);
+            this.Done++;
+            Thread freeTool = new Thread(this.FreeTool);
+            freeTool.Start();
+            Thread wait = new Thread(this.Waiting);
+            wait.Start();
         }
 
         public void Break()
@@ -59,11 +90,36 @@ namespace Workshop6
         public void Waiting()
         {
             this.Status = WorkerStatus.WAITING;
+            this.Tools["right"].Status = ToolStatus.USED;
+            while(this.Tools["left"] == null && this.Tools["right"] != null);
+            {
+                Thread getTool = new Thread(this.GetTool);
+                getTool.Start();
+            }
+            Thread assembling = new Thread(this.Assembling);
+            assembling.Start();
         }
 
-       public void GetTool()
-        {
+        public void GetTool()
+       {
+            if(leftNeigbhor.Tools["right"] != null && leftNeigbhor.Tools["right"].Status == ToolStatus.FREE)
+            {
+                this.Tools["left"] = this.leftNeigbhor.Tools["right"];
+                this.leftNeigbhor.Tools["right"] = null;
+            }
+       }
 
+       public void FreeTool()
+       {
+            this.Tools["right"].Status = ToolStatus.FREE;
+            this.Tools["left"].Status = ToolStatus.FREE;
+            this.leftNeigbhor.Tools["right"] = this.Tools["left"];
+            this.Tools["left"] = null;
+       }
+
+        public void Speak()
+        {
+            Console.WriteLine("I'm {0}, i have in my right hand : {1} {2} and in my left hand {3} and i am currently {4}. My neighbor is {5}", this.Name, this.Tools["right"].Name, this.Tools["right"].Status,  this.Tools["left"], this.Status, this.LeftNeighbor.Name);
         }
     }
 }
